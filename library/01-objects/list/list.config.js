a36d36dd8b052e1b77165d2002ee9a237ee5eb46
Ben Reed <breed@redarrowlabs.com>
2016-10-04 17:22:15 -0500

    poc list object with faker data

--- library/01-objects/list/list.config.js
+++ library/01-objects/list/list.config.js
@@ -1,23 +1,33 @@
 const faker = require('faker/locale/en_US');
+console.log(faker.helpers.createCard());
+
+function createUsers() {
+    var users = [];
+    for (var i = 0; i < 12; i++) {
+        users.push(faker.helpers.createCard());
+    }
+    return users;
+}
 
 module.exports = {
-    title: "Message",
+    title: "List",
     status: "prototype",
     tags: ['objects', 'author:ben'],
     context: {
         text: faker.lorem.sentences(),
+        users: createUsers(),
         cssClasses: {
-            "base": "o-message"
+            "base": "o-list"
         }
     },
     variants: [
         {
-            name: "success",
+            name: "inline",
             status: "prototype",
             context: {
                 text: faker.lorem.sentences(),
                 cssClasses: {
-                    modifier: "success"
+                    modifier: "inline"
                 }
             }
         }
